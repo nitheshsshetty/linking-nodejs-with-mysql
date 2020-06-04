1ef75c9c846cbbbb63160a24884c8fb518309c9e
Nithesh Shetty <17782014+nitheshsshetty@users.noreply.github.com>
2020-06-04 20:11:12 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
-var faker=require('faker')
-var mysql=require('mysql');
+var faker=require('faker') //invoking faker library 
+var mysql=require('mysql'); //invoking mysql library
+
+//connecting to mysql database with credentials
 var connection = mysql.createConnection({
   host     : '*****',
   user     : '****',    
@@ -8,6 +10,7 @@ var connection = mysql.createConnection({
 
 });
 
+//accessing mysql DB via app.js
 var q = 'SELECT CURTIME() as time, CURDATE() as date, NOW() as now';
 connection.query(q, function (error, results, fields) {
   if (error) throw error;
@@ -15,7 +18,16 @@ connection.query(q, function (error, results, fields) {
   console.log(results[0].date);
   console.log(results[0].now);
 });
-//inserting uniquw mail id via faker package
+
+//insert take 1
+var q = 'insert into user (mail) values("aarushi06@gmail.com")';
+connection.query(q, function (error, results, fields) {
+  if (error) throw error;
+  console.log("succesfully added");
+});
+
+
+//inserting unique mail id via faker package
 var person={mail: faker.internet.email()}
 connection.query("Insert into user set ?",person, function (error, result) {
   if (error) throw error;
